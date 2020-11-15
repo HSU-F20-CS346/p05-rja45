8819c27cb4fcf2042959ca1cb23b35c8442c4fb5
rja45 <54596406+rja45@users.noreply.github.com>
2020-11-15 15:57:24 -0800

    Created all API endpoints
    Closes #2
    Closes #3
    Closes #4
    Closes #5
    Closes #6
    Closes #7
    Closes #8
    Closes #9
    Closes #10
    Closes #11
    Closes #12

---
+++ src/Example-RestAPI/Models/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example_RestAPI.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public int InvoiceID { get; set; }
+      
+        public DateTime InvoiceDate { get; set; }
+        public string BillingAddress { get; set; }
+        public string BillingCity { get; set; }
+        public string BillingState { get; set; }
+        public string BillingCountry { get; set; }
+        public string BillingPostalCode { get; set; }
+        public decimal Total { get; set; }
+
+
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+        public virtual Customer Customers { get; set; }
+
+        //Specifies that ArtistId inside Albums is an FK to an Artist
+        //Also, be sure to mark the collection of FK objects as virtual
+        [ForeignKey("InvoiceID")]
+        public virtual ICollection<Invoice_item> Invoice_items { get; set; }
+    }
+}
---
+++ src/Example-RestAPI/Models/Track.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example_RestAPI.Models
+{
+    public class Track
+    {
+        //Primary Key in DB
+        [Key]
+        public int TrackID { get; set; }
+        public string Name { get; set; }
+        public string Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //Specifies that AlbumId is a FK for the Albums table
+        [ForeignKey("Genre")]
+        public int GenreID { get; set; }
+
+        [ForeignKey("Album")]
+        public int AlbumID { get; set; } 
+        
+        [ForeignKey("Media_Type")]
+        public int MediaTypeID { get; set; }
+
+        [ForeignKey("TrackID")]
+        public virtual ICollection<Invoice_item> Invoice_Items { get; set; }
+
+
+    }
+}
